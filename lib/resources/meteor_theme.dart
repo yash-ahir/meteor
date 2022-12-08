0a9b9eb0023566e28525fbaa9775758bbe1cba9f
Yash Ahir <email@yashahir.tech>
2022-12-08 13:34:03 +0530

    Re-implemented with ThemeExtension API

--- lib/resources/meteor_theme.dart
+++ lib/resources/meteor_theme.dart
@@ -2,86 +2,129 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class MeteorTheme extends InheritedWidget {
-  final bool isDarkTheme;
-
+class MeteorTheme extends ThemeExtension<MeteorTheme> {
   const MeteorTheme({
-    Key? key,
-    required Widget child,
-    required this.isDarkTheme,
-  }) : super(key: key, child: child);
+    required this.primaryGradient,
+    required this.errorGradient,
+    required this.extendedGradient,
+    required this.scaffoldBackground,
+    required this.containerBackground,
+    required this.outline,
+    required this.textStyle,
+  });
 
-  Gradient get primaryGradient => isDarkTheme
-      ? const LinearGradient(
-          transform: GradientRotation(pi / 3),
-          colors: [
-            Color(0xFF4FACFE),
-            Color(0xFF00F2FE),
-          ],
-        )
-      : const LinearGradient(
-          transform: GradientRotation(pi / 3),
-          colors: [
-            Color(0xFF005BEA),
-            Color(0xFF00C6FB),
-          ],
-        );
+  final LinearGradient? primaryGradient;
+  final LinearGradient? errorGradient;
+  final LinearGradient? extendedGradient;
+  final Color? scaffoldBackground;
+  final Color? containerBackground;
+  final Color? outline;
+  final TextStyle? textStyle;
 
-  Gradient get errorGradient => isDarkTheme
-      ? const LinearGradient(
-          transform: GradientRotation(pi / 3),
-          colors: [
-            Color(0xFFFA709A),
-            Color(0xFFFF0844),
-          ],
-        )
-      : const LinearGradient(
-          transform: GradientRotation(pi / 3),
-          colors: [
-            Color(0xFFF83F77),
-            Color(0xFFD40033),
-          ],
-        );
-
-  Gradient get extendedGradient => isDarkTheme
-      ? const LinearGradient(
-          colors: [
-            Color(0xFF4FACFE),
-            Color(0xFF02F0FE),
-            Color(0xFFA240E0),
-            Color(0xFFF9709A),
-            Color(0xFFFEE041),
-          ],
-        )
-      : const LinearGradient(
-          colors: [
-            Color(0xFF005BEA),
-            Color(0xFF00C6FB),
-            Color(0xFF8A21CC),
-            Color(0xFFF83F77),
-            Color(0xFFFED80E),
-          ],
-        );
-
-  Color get scaffoldBackground =>
-      isDarkTheme ? const Color(0xFF141414) : const Color(0xFFFFFFFF);
-
-  Color get containerBackground =>
-      isDarkTheme ? const Color(0xFF1F1F1F) : const Color(0xFFF6F6F6);
-
-  Color get outline =>
-      isDarkTheme ? const Color(0xFF6F797A) : const Color(0xFF5F6768);
-
-  TextStyle get textStyle => isDarkTheme
-      ? const TextStyle(color: Color(0xFFFBFBFB))
-      : const TextStyle(color: Color(0xFF111111));
-
-  static MeteorTheme? of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<MeteorTheme>();
+  @override
+  ThemeExtension<MeteorTheme> copyWith({
+    LinearGradient? primaryGradient,
+    LinearGradient? errorGradient,
+    LinearGradient? extendedGradient,
+    Color? scaffoldBackground,
+    Color? containerBackground,
+    Color? outline,
+    TextStyle? textStyle,
+  }) {
+    return MeteorTheme(
+      primaryGradient: primaryGradient ?? this.primaryGradient,
+      errorGradient: errorGradient ?? this.errorGradient,
+      extendedGradient: extendedGradient ?? this.extendedGradient,
+      scaffoldBackground: scaffoldBackground ?? this.scaffoldBackground,
+      containerBackground: containerBackground ?? this.containerBackground,
+      outline: outline ?? this.outline,
+      textStyle: textStyle ?? this.textStyle,
+    );
   }
 
   @override
-  bool updateShouldNotify(covariant MeteorTheme oldWidget) {
-    return isDarkTheme != oldWidget.isDarkTheme;
+  ThemeExtension<MeteorTheme> lerp(
+      ThemeExtension<MeteorTheme>? other, double t) {
+    if (other is! MeteorTheme) {
+      return this;
+    }
+
+    return MeteorTheme(
+      primaryGradient:
+          LinearGradient.lerp(primaryGradient, other.primaryGradient, t),
+      errorGradient: LinearGradient.lerp(errorGradient, other.errorGradient, t),
+      extendedGradient:
+          LinearGradient.lerp(extendedGradient, other.extendedGradient, t),
+      scaffoldBackground:
+          Color.lerp(scaffoldBackground, other.scaffoldBackground, t),
+      containerBackground:
+          Color.lerp(containerBackground, other.containerBackground, t),
+      outline: Color.lerp(outline, other.outline, t),
+      textStyle: other.textStyle,
+    );
   }
+
+  static const light = MeteorTheme(
+    primaryGradient: LinearGradient(
+      transform: GradientRotation(pi / 3),
+      colors: [
+        Color(0xFF005BEA),
+        Color(0xFF00C6FB),
+      ],
+    ),
+    errorGradient: LinearGradient(
+      transform: GradientRotation(pi / 3),
+      colors: [
+        Color(0xFFf77062),
+        Color(0xFFfe5196),
+      ],
+    ),
+    extendedGradient: LinearGradient(
+      colors: [
+        Color(0xFF005BEA),
+        Color(0xFF00C6FB),
+        Color(0xFF8A21CC),
+        Color(0xFFF83F77),
+        Color(0xFFFED80E),
+      ],
+    ),
+    scaffoldBackground: Color(0xFFFFFFFF),
+    containerBackground: Color(0xFFF6F6F6),
+    outline: Color(0xFF5F6768),
+    textStyle: TextStyle(
+      color: Color(0xFF111111),
+    ),
+  );
+
+  static const dark = MeteorTheme(
+    primaryGradient: LinearGradient(
+      transform: GradientRotation(pi / 3),
+      colors: [
+        Color(0xFF4FACFE),
+        Color(0xFF00F2FE),
+      ],
+    ),
+    errorGradient: LinearGradient(
+      transform: GradientRotation(pi / 3),
+      colors: [
+        Color(0xFFFA709A),
+        Color(0xFFFF0844),
+      ],
+    ),
+    extendedGradient: LinearGradient(
+      colors: [
+        Color(0xFF4FACFE),
+        Color(0xFF02F0FE),
+        Color(0xFFA240E0),
+        Color(0xFFF9709A),
+        Color(0xFFFEE041),
+      ],
+    ),
+    scaffoldBackground: Color(0xFF141414),
+    containerBackground: Color(0xFF1F1F1F),
+    outline: Color(0xFF6F797A),
+    textStyle: TextStyle(
+      color: Color(0xFFFBFBFB),
+    ),
+  );
 }
